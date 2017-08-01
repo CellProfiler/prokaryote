f9a04e7ae40df5e4379fecb53d6ca7ef6e645a95
Allen Goodman <allen.goodman@icloud.com>
2017-08-01 14:27:04 -0400

    package cleanup

--- setup.py
+++ setup.py
@@ -1,103 +1,62 @@
-import distutils.command.build
+import codecs
 import os
 import re
-import setuptools
-import setuptools.command.bdist_egg
-import setuptools.command.build_ext
-import setuptools.command.install
+
 import setuptools.dist
 
-setuptools.dist.Distribution({
-    "setup_requires": [
-        "clint",
-        "requests"
-    ]
-})
 
-with open("prokaryote/__init__.py", "r") as fd:
-    version = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", fd.read(), re.MULTILINE).group(1)
-    
-class FetchProkaryoteJar(setuptools.Command):
-    user_options = [
-        ('prokaryote-version=', None, 'Version # for prokaryote.jar')]
-    
-    def initialize_options(self):
-        self.prokaryote_version = None
-        self.build_lib = None
-        self.install_lib = None
-        
-    def finalize_options(self):
-        if self.prokaryote_version is None:
-            self.prokaryote_version = version
-        self.set_undefined_options('build', ('build_lib', 'build_lib'))
-        self.set_undefined_options('install', ('install_lib', 'install_lib'))
-            
-    def run(self):
-        try:
-            import clint.textui
-            import requests
-        except ImportError:
-            raise ImportError
+def read(*directories):
+    pathname = os.path.abspath(os.path.dirname(__file__))
 
-        directory = os.path.join(self.build_lib, "prokaryote")
+    return codecs.open(os.path.join(pathname, *directories), "r").read()
 
-        if not os.path.exists(directory):
-            os.makedirs(directory)
 
-        prokaryote = "{}/prokaryote.jar".format(directory)
+def find_version(*pathnames):
+    data = read(*pathnames)
 
-        resource = "https://github.com/CellProfiler/prokaryote/releases/download/{tag}/prokaryote-{tag}.jar".format(tag=version)
+    matched = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", data, re.M)
 
-        request = requests.get(resource, stream=True)
+    if matched:
+        return matched.group(1)
 
-        with open(prokaryote, "wb") as f:
-            total_length = int(request.headers.get("content-length"))
+    raise RuntimeError("Unable to find version string.")
 
-            chunks = clint.textui.progress.bar(request.iter_content(chunk_size=32768), expected_size=(total_length / 32768) + 1, hide=not self.verbose)
 
-            for chunk in chunks:
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-        
-    def get_outputs(self):
-        return [os.path.join(self.install_lib, "prokaryote", "prokaryote.jar")]
-
-class Install(setuptools.command.install.install):
-    sub_commands = [("fetch_prokaryote_jar", None)] + \
-        setuptools.command.install.install.sub_commands
+__version__ = find_version("prokaryote", "__init__.py")
 
 setuptools.setup(
-        author="Allen Goodman",
-        author_email="allen.goodman@icloud.com",
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: C",
-            "Programming Language :: C++",
-            "Programming Language :: Cython",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.6",
-            "Programming Language :: Python :: 2.7",
-            "Topic :: Scientific/Engineering :: Bio-Informatics",
-            "Topic :: Scientific/Engineering :: Image Recognition",
-            "Topic :: Scientific/Engineering"
-        ],
-        cmdclass={
-            "fetch_prokaryote_jar": FetchProkaryoteJar,
-            "install": Install
-        },
-        include_package_data=True,
-        license="BSD",
-        name="prokaryote",
-        packages=setuptools.find_packages(),
-        setup_requires=[
-            "clint",
-            "requests"
-        ],
-        url="https://github.com/CellProfiler/prokaryote",
-        version=version,
-        zip_safe=False
+    author="Allen Goodman",
+    author_email="allen.goodman@icloud.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Java",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering"
+    ],
+    license="BSD",
+    name="prokaryote",
+    package_data={
+        "prokaryote": [
+            "prokaryote-{}.jar".format(__version__)
+        ]
+    },
+    packages=setuptools.find_packages(),
+    python_requires=">=2.7, <4",
+    url="https://github.com/CellProfiler/prokaryote",
+    version=__version__,
+    zip_safe=False
 )
